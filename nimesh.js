9978fb1be49f816b95cf38c328136a7210bdd7e6
rajiv <nimeshthapa316@gmail.com>
2025-05-06 07:40:26 +0545

    code added

--- nimesh.js
+++ nimesh.js
@@ -5,3 +5,16 @@ const navLinks = document.getElementById('navlinks');
 hamburger.addEventListener('click',()=>{
   navLinks.classList.toggle('active');
 });
+
+const text="Hi, I am Mr.NJT";
+const typetext=document.getElementById("typed-text");
+let index=0;
+const speed=150;
+function type(){
+  if(index<text.length){
+    typetext.textContent+=text.charAt(index);
+    index++;
+    setTimeout(type,speed);
+  }
+}
+type();
