4eecd6654623f746820a7706d005ba0a21d54758
rajiv <nimeshthapa316@gmail.com>
2025-05-08 07:07:35 +0545

    code added

--- nimesh.js
+++ nimesh.js
@@ -1,4 +1,3 @@
-
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navlinks');
 
@@ -18,3 +17,54 @@ function type(){
   }
 }
 type();
+
+const projects = [
+  {
+    image: "image/p1.avif",
+    title: "Project 1",
+    description: "Description of Project 1A creative mobile UI/UX project focusing on accessibility and performance.",
+  },
+  {
+    image: "image/p2.avif",
+    title: "Project 2",
+    description: "Description of Project 2A creative mobile UI/UX project focusing on accessibility and performance.",
+
+  },
+  {
+    image: "image/p1.avif",
+    title: "Project 3",
+    description: "Description of Project 3A creative mobile UI/UX project focusing on accessibility and performance.",
+
+  },
+  {
+    image: "image/p2.avif",
+    title: "Project 4",
+    description: "Description of Project 3A creative mobile UI/UX project focusing on accessibility and performance.",
+
+  },
+  {
+    image: "image/p1.avif",
+    title: "Project 5",
+    description: "Description of Project 3A creative mobile UI/UX project focusing on accessibility and performance.",
+
+  },
+  {
+    image: "image/p2.avif",
+    title: "Project 6",
+    description: "Description of Project 3A creative mobile UI/UX project focusing on accessibility and performance.",
+
+  },
+  
+];
+const Container = document.getElementById("project-container");
+projects .forEach(project => { 
+  const card = document.createElement("div");
+  card.className="project-card";
+  card.innerHTML = `
+    <img src="${project.image}" alt="${project.title}">
+    <h3>${project.title}</h3>
+    <p>${project.description}</p>
+  `;
+  Container.appendChild(card);
+}
+);
